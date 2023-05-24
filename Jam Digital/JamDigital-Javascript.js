c23a0a9d460a1678ec966ef611901d3804f43292
Cahyooo <130074345+Cahyooo@users.noreply.github.com>
2023-05-24 21:37:54 +0700

    Menyelesaikan Javascript

--- Jam Digital/JamDigital-Javascript.js
+++ Jam Digital/JamDigital-Javascript.js
@@ -12,50 +12,39 @@ let min = document.querySelector(".min");
 let hour = document.querySelector(".hour");
 
 let detikDeg = detik * 6;
-let menitDeg = (detik * 60) / 10;
-let jamDeg = (detik * 3600) / 600;
+let menitDeg = (menit * 6) + (detik * 0.1);
+let jamDeg = (jam * 30) + (menit * 0.5) + (detik * (0.5 / 60));
+
 
 let putarSec = `
   @keyframes putarSec {
-    from{transform: translateX(-50%) rotate(${detikDeg}deg)}
+    from {transform: translateX(-50%) rotate(${detikDeg}deg)}
 
     to {transform: translateX(-50%) rotate(${360 + detikDeg}deg)}
 }`;
 
-let untukSec = document.createElement("style");
-untukSec.innerHTML = putarSec;
-document.head.appendChild(untukSec);
-sec.style.animationName = "putarSec";
-sec.style.animationDuration = "60s";
-sec.style.animationIterationCount = "infinite ";
-sec.style.animationTimingFunction = "linear";
-
-let putarMin = `
+ let putarMin = `
   @keyframes putarMin {
-    from{transform: translateX(-50%) rotate(${menitDeg}deg)}
+    from {transform: translateX(-50%) rotate(${menitDeg}deg)}
 
     to {transform: translateX(-50%) rotate(${360 + menitDeg}deg)}
 }`;
 
-let untukMin = document.createElement("style");
-untukMin.innerHTML = putarMin;
-document.head.appendChild(untukMin);
-min.style.animationName = "putarMin";
-min.style.animationDuration = "3600s";
-min.style.animationIterationCount = "infinite ";
-min.style.animationTimingFunction = "linear";
-
 let putarHour = `
   @keyframes putarHour {
-    from{transform: translateX(-50%) rotate(${jamDeg}deg)}
+    from {transform: translateX(-50%) rotate(${jamDeg}deg)}
 
     to {transform: translateX(-50%) rotate(${360 + jamDeg}deg)}
 }`;
 
-let untukHour = document.createElement("style");
-untukHour.innerHTML = putarHour;
-document.head.appendChild(untukHour);
-hour.style.animationName = "putarHour";
-hour.style.animationDuration = "216000s";
-hour.style.animationIterationCount = "infinite ";
-hour.style.animationTimingFunction = "linear";
+let animasiSec = document.createElement('style');
+animasiSec.innerHTML = putarSec
+document.head.appendChild(animasiSec);
+
+let animasiMin = document.createElement('style');
+animasiMin.innerHTML = putarMin
+document.head.appendChild(animasiMin);
+
+let animasiHour = document.createElement('style');
+animasiHour.innerHTML = putarHour
+document.head.appendChild(animasiHour);
