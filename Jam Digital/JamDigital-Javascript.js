0a902ce0eb28196ba2a6c019b66fbc027f4e51f7
Cahyooo <130074345+Cahyooo@users.noreply.github.com>
2023-05-24 20:28:10 +0700

    Tambahan

--- Jam Digital/JamDigital-Javascript.js
+++ Jam Digital/JamDigital-Javascript.js
@@ -19,12 +19,43 @@ let putarSec = `
   @keyframes putarSec {
     from{transform: translateX(-50%) rotate(${detikDeg}deg)}
 
-    to {transform: translateX(-50%) rotate(${detikDeg-0.1}deg)}
+    to {transform: translateX(-50%) rotate(${360 + detikDeg}deg)}
 }`;
 
 let untukSec = document.createElement("style");
 untukSec.innerHTML = putarSec;
 document.head.appendChild(untukSec);
 sec.style.animationName = "putarSec";
-sec.style.animationDuration = "10s";
-sec.style.animationIterationCount = "infinite";
+sec.style.animationDuration = "60s";
+sec.style.animationIterationCount = "infinite ";
+sec.style.animationTimingFunction = "linear";
+
+let putarMin = `
+  @keyframes putarMin {
+    from{transform: translateX(-50%) rotate(${menitDeg}deg)}
+
+    to {transform: translateX(-50%) rotate(${360 + menitDeg}deg)}
+}`;
+
+let untukMin = document.createElement("style");
+untukMin.innerHTML = putarMin;
+document.head.appendChild(untukMin);
+min.style.animationName = "putarMin";
+min.style.animationDuration = "3600s";
+min.style.animationIterationCount = "infinite ";
+min.style.animationTimingFunction = "linear";
+
+let putarHour = `
+  @keyframes putarHour {
+    from{transform: translateX(-50%) rotate(${jamDeg}deg)}
+
+    to {transform: translateX(-50%) rotate(${360 + jamDeg}deg)}
+}`;
+
+let untukHour = document.createElement("style");
+untukHour.innerHTML = putarHour;
+document.head.appendChild(untukHour);
+hour.style.animationName = "putarHour";
+hour.style.animationDuration = "216000s";
+hour.style.animationIterationCount = "infinite ";
+hour.style.animationTimingFunction = "linear";
